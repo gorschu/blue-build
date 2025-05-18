4ff00b2bb10da65cae7cf367f0f24d9660f52abf
Gordon Schulz <gordon@gordonschulz.de>
2025-05-18 11:33:52 +0200

    chore(recipes): (Determinate) Nix is completely broken for now, anyway. Remove it

--- files/scripts/determinate-nix.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# install determinate nix
-set -oue pipefail
-curl -fsSL https://install.determinate.systems/nix | sh -s -- install --no-start-daemon --determinate --no-confirm
